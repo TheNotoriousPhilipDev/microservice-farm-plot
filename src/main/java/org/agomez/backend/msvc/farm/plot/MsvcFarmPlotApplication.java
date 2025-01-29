643cf4edbc6c8c44a947efdb43f00a4b23003ba7
andres <afgomez0622@gmail.com>
2025-01-28 21:24:10 -0500

    first commit

---
+++ src/main/java/org/agomez/backend/msvc/farm/plot/MsvcFarmPlotApplication.java
@@ -0,0 +1,13 @@
+package org.agomez.backend.msvc.farm.plot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcFarmPlotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcFarmPlotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/agomez/backend/msvc/farm/plot/MsvcFarmPlotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.agomez.backend.msvc.farm.plot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcFarmPlotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
